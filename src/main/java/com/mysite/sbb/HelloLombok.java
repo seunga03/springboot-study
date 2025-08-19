5a199f55f42b3d4e4588cbb598008969dc102a29
seunga03 <seungalee220@gmail.com>
2025-08-19 14:40:21 +0900

    ch01-04: lombok required args constructor 만들기

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,37 +1,23 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@Setter
+@RequiredArgsConstructor
 public class HelloLombok {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
 
     /*
-    * public void setHello(String hello) {
+    * public HelloLombok(String hello, int lombok) {
     *   this.hello = hello;
-    * }
-    *
-    * public void setLombok(int lombok) {
     *   this.lombok = lombok;
-    * }
-    *
-    * public String getHello() {
-    *   return this.hello;
-    * }
-    *
-    * public int getLombok() {
-    *   return this.lombok;
     * }
     * */
 
     public static void main(String[] args) {
-        HelloLombok helloLombok = new HelloLombok();
-        helloLombok.setHello("헬로");
-        helloLombok.setLombok(5);
-
+        HelloLombok helloLombok = new HelloLombok("헬로",  5);
         System.out.println(helloLombok.getHello());
         System.out.println(helloLombok.getLombok());
 
